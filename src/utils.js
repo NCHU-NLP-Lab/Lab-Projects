f172c1f956893433c1047a738099c24555efd78e
p208p2002 <p208p2002@gmail.com>
2021-08-13 14:58:40 +0800

    no message

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 const yaml = require('js-yaml');
 
 export async function yamlParser(path){
-    let parseResult = await fetch(path)
+    let parseResult = await fetch(process.env.PUBLIC_URL + path)
     .then(r => r.text())
     .then(text => {
       return yaml.load(text)
